950548142effeade96369e253da87c0ff0ac5930
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2022-09-01 23:42:47 -0400

    Reduce log y pow mejorado

--- Expression/ReducePowLogarithm.cs
+++ Expression/ReducePowLogarithm.cs
@@ -14,8 +14,14 @@ internal static class ReducePowLogarithm
         ExpressionType? aux = ReducePowSimple(binary);
         if (aux is not null) return aux;
 
-        Pow? exp = binary.Left as Pow;
-        if (exp is not null) return ReducePow(Pow.DeterminatePow(exp.Left, exp.Right * binary.Right));
+        Pow? pow = binary.Left as Pow;
+        if (pow is not null) return ReducePow(Pow.DeterminatePow(pow.Left, pow.Right * binary.Right));
+
+        Log? log = binary.Right as Log;
+        if (log is not null)
+        {
+            if (log.Left.Equals(binary.Left)) return log.Right;
+        }
 
         return binary;
     }
@@ -37,11 +43,38 @@ internal static class ReducePowLogarithm
         return null;
     }
 
+    /// <summary>
+    /// Reducir un logaritmo
+    /// </summary>
+    /// <param name="binary">Expresion binaria</param>
+    /// <returns>Expresion resultante</returns>
     internal static ExpressionType ReduceLogarithm(BinaryExpression binary)
+    {
+        ExpressionType? aux = ReduceLogarithmSimple(binary);
+        if (aux is not null) return aux;
+
+        Pow? pow = binary.Right as Pow;
+        if (pow is not null)
+        {
+            if (pow.Left.Equals(binary.Left)) return pow.Right;
+        }
+
+        return binary;
+    }
+
+    /// <summary>
+    /// Determinar si un logaritmo
+    /// se puede reducir dadas caracteristicas simples
+    /// </summary>
+    /// <param name="binary">Expresion binaria</param>
+    /// <returns>Expresion resultante(si es null es que no se pudo reducir)</returns>
+    private static ExpressionType? ReduceLogarithmSimple(BinaryExpression binary)
     {
         if (binary.Right.Equals(new NumberExpression(RealNumbers.Real1)))
             return new NumberExpression(RealNumbers.Real0);
 
-        return binary;
+        if (binary.Right.Equals(binary.Left)) return new NumberExpression(RealNumbers.Real1);
+
+        return null;
     }
 }
